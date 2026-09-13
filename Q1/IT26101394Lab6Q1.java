33d37cc76742eb2a6749d11c59c4101597fd8762
IT26101394 <IT26101394@my.sliit.lk>
2026-09-13 15:00:06 +0530

    ADD lab to q1

---
+++ Q1/IT26101394Lab6Q1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class IT26101394Lab6Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        double num = scanner.nextDouble();
+        
+        double square = num * num;
+        double squareRoot = Math.sqrt(num);
+        
+        System.out.println("The square of " + num + " is: " + square);
+        System.out.println("The square root of " + num + " is: " + squareRoot);
+        
+       
+    }
+}
