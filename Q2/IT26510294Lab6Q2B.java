97c45600fc41a9903d536228ba1eae123ac2a471
IT26510294 <it26510294@my.sliit.lk>
2026-09-13 08:47:42 +0530

    Add files via upload

---
+++ Q2/IT26510294Lab6Q2B.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class IT26510294Lab6Q2B {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.print("Enter number " + i + ": ");
+            int number = input.nextInt();
+
+            System.out.print(number + " ");
+            i++;
+        }
+
+        input.close();
+    }
+}
---
+++ Q2/IT26510294Lab6Q2C.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class IT26510294Lab6Q2C {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int i = 1;
+        int sum = 0;
+
+        while (i <= 10) {
+            System.out.print("Enter number " + i + ": ");
+            int number = input.nextInt();
+
+            sum = sum + number;
+            i++;
+        }
+
+        double average = sum / 10.0;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+
+        input.close();
+    }
+}
+```
